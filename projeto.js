f0e950583a576db26a2e4ca2f37c45270fdb134a
Gian F Maciel <gian.f.maciel@gmail.com>
2024-10-24 21:53:21 -0300

    commit projeto final

--- projeto.js
+++ projeto.js
@@ -1,66 +1,86 @@
-let nomeUsuario = prompt("Qual é seu nome ?")
-let cidadeUsuario = prompt("Qual cidade você está ?")
-let opcaoMenu = Number(prompt("Digite o número da opção desejada: 1-Cadastrar Abrigo. 2- Listar Abrigos. 3- Procurar Abrigo. 4- Sair."))
+const nomeUsuario = prompt("Qual é seu nome ?")
+const cidadeUsuario = prompt("Qual cidade você está ?")
 
-let listaAbrigo = ""
-
-let abrigos = [
+const abrigos = [
     {
-        codigoAbrigo: 1,
         nomeAbrigo: "Lar de Caridade",
-        enderecoAbrigo: "São Leopoldo",
+        enderecoAbrigo: "Rua Lisboa n. 120",
         telefoneAbrigo: 35728599,
-        capacidadeAbrigo: 35
+        capacidadeAbrigo: 35,
+        cidade: "São Leopoldo"
     },
     {
-        codigoAbrigo: 2,
         nomeAbrigo: "União",
-        enderecoAbrigo: "Novo Hamburgo",
+        enderecoAbrigo: "Rua America n.50",
         telefoneAbrigo: 35684588,
-        capacidadeAbrigo: 40
+        capacidadeAbrigo: 40,
+        cidade: "Novo Hamburgo"
     },
     {
-        codigoAbrigo: 3,
         nomeAbrigo: "Abraço de Jesus",
-        enderecoAbrigo: "Canoas",
+        enderecoAbrigo: "Rua Independência n. 355",
         telefoneAbrigo: 35729877,
-        capacidadeAbrigo: 50
+        capacidadeAbrigo: 50,
+        cidade: "Canoas"
     },
     {
-        codigoAbrigo: 4,
         nomeAbrigo: "Lar da Esperança",
-        enderecoAbrigo: "Porto Alegre",
+        enderecoAbrigo: "Rua Catedral n. 420",
         telefoneAbrigo: 35723125,
-        capacidadeAbrigo: 45
+        capacidadeAbrigo: 45,
+        cidade: "Porto Alegre"
     },
 ]
 
-const cadastrarAbrigo = (cadastramento) => {
-    if (opcaoMenu === 1){
-        for (let i = 0; i < abrigos.codigoAbrigo; i++){
-            codigoAbrigo = i + 1
-            abrigos.push(codigoAbrigo) 
-        }
-        let nomeAbrigo = prompt("Qual é o nome do seu abrigo ?")
-        abrigos.push(nomeAbrigo)
-        let enderecoAbrigo = prompt("Qual é a cidade que reside seu abrigo ?")
-        abrigos.push(enderecoAbrigo)
-        let telefoneAbrigo = Number(prompt("Qual é o número do telefone do abrigo ?"))
-        abrigos.push(telefoneAbrigo)
-        let capacidadeAbrigo = Number(prompt("Qual é a capacidade de lotação do seu abrigo ?"))
-        abrigos.push(capacidadeAbrigo)
-        cadastramento = abrigos
-        console.log(abrigos.length)
-    }
-    else if (opcaoMenu === 2) {
-        for (let i = 0; i < abrigos.length; i++ ) {
-            listaAbrigo = i.length
-            console.log(listaAbrigo)
-        }
+function menu () {
+    const opcaoMenu = Number(prompt(`Digite o número da opção desejada:
+        1- Cadastrar Abrigo.
+        2- Listar Abrigos.
+        3- Procurar Abrigo.
+        4- Sair.`))
 
-    }
-    return 
-    
+
+        if (opcaoMenu === 1){
+            cadastrarAbrigo ()
+
+        } else if (opcaoMenu === 2) {
+            listaAbrigo()
+            
+        } else if (opcaoMenu === 3) {
+            procurarAbrigo()
+
+        } else if (opcaoMenu === 4) {
+            return
+        }
+        menu()
 }
 
+    function cadastrarAbrigo () {
+        const novo = []
+        novo.nomeAbrigo = prompt("Qual é o nome do seu abrigo ?")
+        novo.enderecoAbrigo = prompt("Qual é a cidade que reside seu abrigo ?")
+        novo.telefoneAbrigo = Number(prompt("Qual é o número do telefone do abrigo ?"))
+        novo.capacidadeAbrigo = Number(prompt("Qual é a capacidade de lotação do seu abrigo ?"))
+        abrigos.push(novo)
+    
+        console.log(abrigos)
+    }
 
+    function listaAbrigo () {
+        console.log('--------------------')
+        console.log('LISTAGEM DE ABRIGOS:')
+        console.log('--------------------')
+        console.log('CÓDIGO |         NOME         |         ENDEREÇO           |   TELEFONE   |  CAPACIDADE | CIDADE')
+        for (let i = 0; i < abrigos.length; i++ ) {
+            console.log(i+1,abrigos[i].nomeAbrigo,abrigos[i].enderecoAbrigo,abrigos[i].telefoneAbrigo,abrigos[i].capacidadeAbrigo,abrigos[i].cidade)
+        }
+
+
+    }
+
+    function procurarAbrigo () {
+
+    }
+
+
+menu()
